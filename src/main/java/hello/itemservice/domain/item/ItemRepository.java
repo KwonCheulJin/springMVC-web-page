72dc7fcb5ad42ddc0ab4a1aef44cc85257caf37c
charles <chkftm12@gmail.com>
2021-10-03 14:07:05 +0900

    기본 html thymeleaf로 변환

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -1,4 +1,4 @@
-package hello.itemservice.domain;
+package hello.itemservice.domain.item;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -2,6 +2,8 @@ package hello.itemservice.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
